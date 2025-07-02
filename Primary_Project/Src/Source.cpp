c01c85312e8d6b03787e2f5e42bb099164d810b7
Mark4401 <194540937+Mark4401@users.noreply.github.com>
2025-07-02 13:38:06 -0500

    simple argument Processing output

--- Primary_Project/Src/Source.cpp
+++ Primary_Project/Src/Source.cpp
@@ -2,9 +2,33 @@
 
 using namespace std;
 
+void argPars(int argc, char* argv[])
+{
+	int n = 0;
+	bool AllArgsPassed = false;
+
+
+	while (!AllArgsPassed)
+	{
+		if (n >= argc)
+		{
+			AllArgsPassed = true;
+			break;
+		}
+
+		string flag(argv[n]);
+
+		cout << n << ": " << flag << endl;
+
+		n++; // Go to next argument
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	cout << "Hello World!\n";
+
+	argPars(argc, argv);
 
 	return 0;
 }
